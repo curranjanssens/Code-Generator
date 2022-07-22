95cc3acedc5dc379a17f5f0d4442cb86d608ec7c
Jacob Andersen <jacob@algorithmjunkie.com>
2022-07-22 10:40:49 +0800

    fix my mans last name part 2

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -3,7 +3,7 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6">
             <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
                 <div class="flex justify-start lg:w-0 lg:flex-1">
-                    <nuxt-link to="/"><h1 class="text-xl font-bold text-gray-500">AI <small class="text-xs">by CJanssen</small></h1></nuxt-link>
+                    <nuxt-link to="/"><h1 class="text-xl font-bold text-gray-500">AI <small class="text-xs">by CJanssens</small></h1></nuxt-link>
                 </div>
 
                 <nuxt-link to="/sql" class="text-base font-medium text-gray-500 hover:text-gray-900">SQL Code Generator</nuxt-link>
